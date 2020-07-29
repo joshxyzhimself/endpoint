99926c42958a2c9ad614f189d0119c46fad6c61e
joshxyzhimself <joshxyzhimself@gmail.com>
2020-07-29 11:40:19 +0800

    Support application/octet-stream responses

--- client/index.js
+++ client/index.js
@@ -33,21 +33,42 @@ const request = async (options) => {
   try {
     const response = await fetch(url, init);
 
-    const responseContentType = response.headers.get('content-type');
+    const response_content_type = response.headers.get('content-type');
 
-    if (typeof responseContentType !== 'string') {
-      throw new Error('fetch(options), Response content-type must be a string.');
-    }
-    if (responseContentType.includes('application/json') === false) {
-      throw new Error('fetch(options), Response content-type must be a application/json.');
+    if (typeof response_content_type !== 'string') {
+      throw new Error('fetch(options), response_content_type must be a string.');
     }
 
-    try {
-      const response2 = await response.json();
-      return response2;
-    } catch (e) {
-      console.error(e);
-      throw new Error(`fetch(options), Parsing error. ${e.message}`);
+    switch (response_content_type) {
+      case 'application/json': {
+        try {
+          const response_json = await response.json();
+          return response_json;
+        } catch (e) {
+          console.error(e);
+          throw new Error(`fetch(options), application/json parsing error. ${e.message}`);
+        }
+      }
+      case 'application/octet-stream': {
+        try {
+          const response_content_disposition = response.headers.get('content-disposition');
+          const response_blob_filename = response_content_disposition.substring(22, response_content_disposition.length - 1);
+          const response_blob = await response.blob();
+          const response_blob_object_url = window.URL.createObjectURL(response_blob);
+          const link_element = document.createElement('a');
+          link_element.href = response_blob_object_url;
+          link_element.download = response_blob_filename;
+          link_element.click();
+          setTimeout(() => window.URL.revokeObjectURL(response_blob_object_url), 250);
+          return {};
+        } catch (e) {
+          console.error(e);
+          throw new Error(`fetch(options), application/octet-stream parsing error. ${e.message}`);
+        }
+      }
+      default: {
+        throw new Error(`fetch(options), Unexpected response_content_type, got "${response_content_type}"`);
+      }
     }
   } catch (e) {
     console.error(e);
