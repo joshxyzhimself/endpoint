449eb50d19db2480175ecca46ecb947505224f87
joshxyzhimself <joshxyzhimself@gmail.com>
2020-10-19 23:20:56 +0800

    remap dirs

---
+++ extras/browser/assert.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Boolean} value
+ * @param {String} message
+ */
+const assert = (value, message) => {
+  if (typeof value !== 'boolean') {
+    throw new Error('assert(value, message), "value" must be a boolean.');
+  }
+  if (typeof message !== 'string') {
+    throw new Error('assert(value, message), "message" must be a string.');
+  }
+  if (value !== true) {
+    throw new Error(message);
+  }
+};
+
+export default assert;
