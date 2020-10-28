ce1b473762e7cd498e910deecdf22b6541e99474
joshxyzhimself <joshxyzhimself@gmail.com>
2020-10-28 14:24:27 +0800

    add use_breakpoint

---
+++ extras/browser/use_breakpoint.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const get_breakpoint = () => {
+  if (window.innerWidth >= 1280) {
+    return 'xl';
+  }
+  if (window.innerWidth >= 1024) {
+    return 'lg';
+  }
+  if (window.innerWidth >= 768) {
+    return 'md';
+  }
+  if (window.innerWidth >= 640) {
+    return 'sm';
+  }
+  return 'xs';
+};
+
+function use_breakpoint() {
+  const [breakpoint, set_breakpoint] = useState(get_breakpoint());
+  useEffect(() => {
+    const handler = () => {
+      const current_breakpoint = get_breakpoint();
+      if (breakpoint !== current_breakpoint) {
+        set_breakpoint(current_breakpoint);
+      }
+    };
+    window.addEventListener('resize', handler);
+    return () => {
+      window.removeEventListener('resize', handler);
+    };
+  }, [breakpoint]);
+  return breakpoint;
+}
+
+export default use_breakpoint;
