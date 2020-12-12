fafd1d3188fac5481d1d5dcff70097014e6c4314
joshxyzhimself <joshxyzhimself@gmail.com>
2020-12-12 17:04:53 +0800

    update assert

--- extras/browser/assert.js
+++ extras/browser/assert.js
@@ -5,22 +5,25 @@ class AssertionError extends Error {
     if (Error.captureStackTrace instanceof Function) {
       Error.captureStackTrace(this, AssertionError);
     }
+    this.name = 'AssertionError';
   }
 }
 
 /**
  * @param {Boolean} value
- * @param {String|undefined} message
+ * @param {String|void} message
  */
 const assert = (value, message) => {
   if (typeof value !== 'boolean') {
-    throw new AssertionError('assert(value, message?), "value" must be a boolean.');
+    throw new AssertionError('on assert(value, message), "value" must be a boolean.');
   }
-  if (message !== undefined && typeof message !== 'string') {
-    throw new AssertionError('assert(value, message?), "message" must be a string.');
+  if (message !== undefined) {
+    if (typeof message !== 'string') {
+      throw new AssertionError('on assert(value, message), "message" must be a string.');
+    }
   }
   if (value === false) {
-    throw new AssertionError(`assert(value, message?), ${message || 'assertion failed.'}`);
+    throw new AssertionError(`${message || 'assertion failed.'}`);
   }
 };
 
