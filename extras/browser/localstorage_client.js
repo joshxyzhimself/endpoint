8731fe0c3d12a29882437b3d1b91cafe99f518e9
joshxyzhimself <joshxyzhimself@gmail.com>
2020-12-17 13:57:55 +0800

    save

---
+++ extras/browser/localstorage_client.js
@@ -0,0 +1,24 @@
+
+const assert = require('endpoint/extras/common/assert');
+const emitter = require('endpoint/extras/common/emitter');
+
+function localstorage_client() {
+  const events = new emitter();
+  window.onstorage = (event) => {
+    if (event.key === 'message') {
+      const raw_message = event.newValue;
+      const message = JSON.parse(raw_message);
+      events.emit('message', message);
+    }
+  };
+  const send = (message) => {
+    assert(message instanceof Object);
+    const raw_message = JSON.stringify(message);
+    localStorage.setItem('message', raw_message);
+  };
+  this.send = send;
+  this.on = events.on.bind(events);
+  this.off = events.off.bind(events);
+}
+
+module.exports = localstorage_client;
