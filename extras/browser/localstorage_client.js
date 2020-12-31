d813c2a12053bf996d7adf0b3087b8f7dd037545
joshxyzhimself <joshxyzhimself@gmail.com>
2020-12-31 14:44:53 +0800

    lsc now uses 'broadcast', has set, get, remove

--- extras/browser/localstorage_client.js
+++ extras/browser/localstorage_client.js
@@ -5,20 +5,41 @@ const emitter = require('../common/emitter');
 function localstorage_client() {
   const events = new emitter();
   window.onstorage = (event) => {
-    if (event.key === 'message') {
-      const raw_message = event.newValue;
-      const message = JSON.parse(raw_message);
-      events.emit('message', message);
+    if (event.key === 'broadcast') {
+      const encoded_message = event.newValue;
+      const message = JSON.parse(encoded_message);
+      events.emit('broadcast', message);
     }
   };
-  const send = (message) => {
+  const broadcast = (message) => {
     assert(message instanceof Object);
-    const raw_message = JSON.stringify(message);
-    localStorage.setItem('message', raw_message);
+    const encoded_message = JSON.stringify(message);
+    localStorage.setItem('broadcast', encoded_message);
   };
-  this.send = send;
+  const set = (key, value) => {
+    assert(typeof key === 'string');
+    const encoded_value = JSON.stringify(value);
+    localStorage.setItem(key, encoded_value);
+  };
+  const get = (key) => {
+    assert(typeof key === 'string');
+    const encoded_value = localStorage.getItem(key);
+    if (typeof encoded_value === 'string') {
+      const value = JSON.parse(encoded_value);
+      return value;
+    }
+    return null;
+  };
+  const remove = (key) => {
+    assert(typeof key === 'string');
+    localStorage.removeItem(key);
+  };
+  this.broadcast = broadcast;
   this.on = events.on.bind(events);
   this.off = events.off.bind(events);
+  this.set = set;
+  this.get = get;
+  this.remove = remove;
 }
 
 module.exports = localstorage_client;
