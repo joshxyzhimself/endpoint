53a757f98a95e42eeb6e9c9b83c42b46d7d2f0d6
joshxyzhimself <joshxyzhimself@gmail.com>
2021-05-16 09:16:17 +0800

    v5.5.0: improve letsencrypt.sh

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -1,44 +1,76 @@
 #!/bin/bash
 
-DOMAIN="$1"
-
-if [ -z "$DOMAIN" ]; then
-    echo "Error: DOMAIN parameter required." >&2
+if [ $# -lt 1 ]; then
+    echo "Error: at least one (1) DOMAIN parameter is required." >&2
     exit 1
 fi
 
-if ! [ -x "$(command -v certbot --version)" ]; then
-    apt-get update
-    apt-get install software-properties-common
+certbot_path=$(command -v certbot);
+echo "==================== certbot_path ${certbot_path}"
+haproxy_path=$(command -v haproxy);
+echo "==================== haproxy_path ${haproxy_path}"
+
+if [ ! -x "$certbot_path" ]; then
+    echo
+    echo "==================== INSTALL CERTBOT"
+    apt update
+    apt install software-properties-common
     add-apt-repository -y universe
-    apt-get update
-    apt-get install certbot -y
+    apt update
+    apt install certbot -y
     echo "Installed: certbot"
 fi
 
-systemctl stop haproxy
-echo "Stopping: haproxy"
+if [ ! -x "$haproxy_path" ]; then
+    echo
+    echo "==================== INSTALL HAPROXY"
+    apt update
+    apt install haproxy -y
+    echo "Installed: haproxy"
+fi
 
-mkdir -p /etc/letsencrypt/live/${DOMAIN}/
-echo "Created: /etc/letsencrypt/live/${DOMAIN}/"
+if [ -x "$haproxy_path" ]; then
+    echo
+    echo "==================== STOP HAPROXY"
+    systemctl stop haproxy
+    echo "Stopped: haproxy"
+fi
 
-sudo certbot certonly --standalone --noninteractive --preferred-challenges http --domain ${DOMAIN} --email admin@${DOMAIN} --agree-tos --no-eff-email
-echo "Created: /etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
-echo "Created: /etc/letsencrypt/live/${DOMAIN}/privkey.pem"
-echo "Created: /etc/letsencrypt/live/${DOMAIN}/cert.pem"
+if [ -x "$haproxy_path" ]; then
+    echo
+    echo "==================== HAPROXY TLS DIR"
+    mkdir -p /etc/haproxy/tls/
+    echo "Created: /etc/haproxy/tls/"
+fi
 
-mkdir -p /etc/haproxy/tls/
-echo "Created: /etc/haproxy/tls/"
-
-cat /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /etc/letsencrypt/live/${DOMAIN}/privkey.pem > /etc/haproxy/tls/${DOMAIN}.pem
-echo "Created /etc/haproxy/tls/${DOMAIN}.pem"
-echo "Usage: bind *:443 ssl crt /etc/haproxy/tls/"
-echo "Usage: https://serverfault.com/a/937946"
+for DOMAIN in "$@"
+do
+    echo
+    echo "==================== DOMAIN: ${DOMAIN}"
+    mkdir -p /etc/letsencrypt/live/${DOMAIN}/
+    echo "Created: /etc/letsencrypt/live/${DOMAIN}/"
+    sudo certbot certonly --standalone --noninteractive --preferred-challenges http --domain ${DOMAIN} --register-unsafely-without-email --quiet
+    echo "Created: /etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
+    echo "Created: /etc/letsencrypt/live/${DOMAIN}/privkey.pem"
+    echo "Created: /etc/letsencrypt/live/${DOMAIN}/cert.pem"
+    if [ -x "$haproxy_path" ]; then
+        cat /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /etc/letsencrypt/live/${DOMAIN}/privkey.pem > /etc/haproxy/tls/${DOMAIN}.pem
+        echo "Created /etc/haproxy/tls/${DOMAIN}.pem"
+    fi
+done
 
 if ! [ -f "/etc/haproxy/tls/dhparam.pem" ]; then
+    echo
+    echo "==================== CREATE DHPARAM"
     openssl dhparam -outform PEM -out /etc/haproxy/tls/dhparam.pem 2048
     echo "Created: /etc/haproxy/tls/dhparam.pem"
 fi
 
-systemctl start haproxy
-echo "Starting: haproxy"
+if [ -x "$haproxy_path" ]; then
+    echo
+    echo "==================== START HAPROXY"
+    systemctl start haproxy
+    echo "Started: haproxy"
+    echo "Usage: bind *:443 ssl crt /etc/haproxy/tls/"
+    echo "Usage: https://serverfault.com/a/937946"
+fi
