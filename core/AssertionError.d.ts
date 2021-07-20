dec17a65291d277f3e858a2ac02066c59f124bd8
joshxyzhimself <joshxyzhimself@gmail.com>
2021-07-20 22:14:06 +0800

    v7.2.1 - update: assertionerror types

--- core/AssertionError.d.ts
+++ core/AssertionError.d.ts
@@ -6,7 +6,11 @@ export interface AssertionErrorJSON {
   stack: string,
 }
 
-export class AssertionError {
+export class AssertionError extends Error {
+  name: string;
+  code: string;
+  message: string;
+  stack: string;
   constructor(code: string, message: string) : void;
   toJSON() : AssertionErrorJSON;
 }
