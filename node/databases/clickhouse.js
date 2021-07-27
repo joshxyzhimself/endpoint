30ee29d14eafcd80bbfa19bd2dccb8f72819b2e7
joshxyzhimself <joshxyzhimself@gmail.com>
2021-07-27 13:12:10 +0800

    v7.4.1 - clickhouse: 30mb buffer limit

--- node/databases/clickhouse.js
+++ node/databases/clickhouse.js
@@ -13,14 +13,14 @@ const create_ch_client = (clickhouse_host, clickhouse_port, clickhouse_database,
   assert(typeof clickhouse_database === 'string');
   assert(clickhouse_options === undefined || clickhouse_options instanceof Object);
   const clickhouse_options_stringified = querystring.stringify({
+    max_threads: os.cpus().length,
+    max_insert_threads: os.cpus().length,
     date_time_input_format: 'best_effort',
     date_time_output_format: 'iso',
     mutations_sync: 2,
     max_query_size: 100000000000,
     max_block_size: 100000000000,
     http_max_uri_size: 100000000000,
-    max_threads: os.cpus().length,
-    max_insert_threads: os.cpus().length,
     max_insert_block_size: 100000000000,
     min_insert_block_size_rows: 100000000000,
     min_insert_block_size_bytes: 100000000000,
@@ -36,11 +36,15 @@ const create_ch_client = (clickhouse_host, clickhouse_port, clickhouse_database,
    */
   const query = async (request_data) => {
     assert(typeof request_data === 'string' || request_data instanceof Buffer);
+    const request_buffer = typeof request_data === 'string'
+      ? Buffer.from(request_data)
+      : request_data;
+    assert(request_buffer.byteLength <= 30000000, 'ERR_BUFFER_GTE_30MB', 'Buffer GTE 30MB.');
     const response = await undici2.request({
       method: 'POST',
       url: clickhouse_url,
       headers: { 'x-clickhouse-format': 'JSON' },
-      buffer: request_data,
+      buffer: request_buffer,
     });
     return response;
   };
